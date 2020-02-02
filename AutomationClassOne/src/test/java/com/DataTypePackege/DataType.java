09ec64006906c1e36be127838db92c7b22f3cfb4
14694 <14694@DESKTOP-QLRTRDC>
2020-02-02 16:46:28 -0500

    first push

---
+++ AutomationClassOne/src/test/java/com/DataTypePackege/DataType.java
@@ -0,0 +1,33 @@
+package com.DataTypePackege;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x= 1;
+		int y= 2;
+		int i= 3;
+		x= 5;
+		double a = 1.555555555555555555555;
+		
+		char z = '1';
+		
+		String name = "S";
+		
+		
+		boolean b = true;
+		boolean c = false;
+		
+		System.out.println("2  3");
+		
+		
+		
+
+	
+		
+	}
+
+
+
+}
